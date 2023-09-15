d06a6557da379fdd7008d1a8c4ccc8a63bb328b5
Michael Abbott <michael.abbott@diamond.ac.uk>
2023-09-15 10:35:54 +0100

    Work in progress on tests

--- test-gddr6_phy/tools/registers.py
+++ test-gddr6_phy/tools/registers.py
@@ -4,8 +4,6 @@ import os
 
 from fpga_lib.driver import driver
 
-driver.VERBOSE = True
-
 here = os.path.dirname(__file__)
 register_defines = os.path.join(here, '../vhd/register_defines.in')
 gddr6_defines = os.path.join(here, '../../gddr6/vhd/gddr6_register_defines.in')
@@ -13,3 +11,23 @@ gddr6_defines = os.path.join(here, '../../gddr6/vhd/gddr6_register_defines.in')
 raw_regs = driver.RawRegisters('ifc_1412-gddr6', 1)
 
 regs = driver.Registers(raw_regs, gddr6_defines, register_defines)
+sg = regs.SYS.GDDR6
+
+
+def read_riu(nibble, reg):
+    riu = sg.RIU
+    address = (nibble << 6) | (reg & 0x3F)
+
+    riu._write_fields_wo(ADDRESS = address, WRITE = 0, VTC = 1)
+    result = riu._get_fields()
+    assert not result.TIMEOUT
+    return result.DATA
+
+def write_riu(nibble, reg, value):
+    riu = sg.RIU
+    address = (nibble << 6) | (reg & 0x3F)
+    riu._write_fields_wo(DATA = value, ADDRESS = address, WRITE = 1, VTC = 1)
+    result = riu._get_fields()
+    assert not result.TIMEOUT
+
+__all__ = ['sg', 'read_riu', 'write_riu']
