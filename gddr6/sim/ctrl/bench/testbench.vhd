1c8e72edfe3712eafc152ec14236a07d5e6732fb
Michael Abbott <michael.abbott@diamond.ac.uk>
2024-04-11 15:17:50 +0100

    Initial version of integration test bench for gddr6_ctrl

---
+++ gddr6/sim/ctrl/bench/testbench.vhd
@@ -0,0 +1,116 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.textio.all;
+
+use work.support.all;
+
+use work.gddr6_ctrl_core_defs.all;
+use work.gddr6_defs.all;
+
+entity testbench is
+end testbench;
+
+
+architecture arch of testbench is
+    signal clk : std_ulogic := '0';
+
+    procedure clk_wait(count : natural := 1) is
+    begin
+        for i in 1 to count loop
+            wait until rising_edge(clk);
+        end loop;
+    end;
+
+    signal tick_count : natural;
+
+    constant SHORT_REFRESH_COUNT : natural := 100;
+    constant LONG_REFRESH_COUNT : natural := 10;
+
+    signal ctrl_setup : ctrl_setup_t;
+    signal ctrl_status : ctrl_status_t;
+    signal axi_request : axi_request_t;
+    signal axi_response : axi_response_t;
+    signal phy_ca : phy_ca_t;
+    signal phy_dq_out : phy_dq_out_t;
+    signal phy_dq_in : phy_dq_in_t;
+
+    procedure write(message : string := "") is
+        variable linebuffer : line;
+    begin
+        write(linebuffer, message);
+        writeline(output, linebuffer);
+    end;
+
+begin
+    clk <= not clk after 2 ns;
+
+    ctrl : entity work.gddr6_ctrl generic map (
+        SHORT_REFRESH_COUNT => SHORT_REFRESH_COUNT,
+        LONG_REFRESH_COUNT => LONG_REFRESH_COUNT
+    ) port map (
+        clk_i => clk,
+        ctrl_setup_i => ctrl_setup,
+        ctrl_status_o => ctrl_status,
+        axi_request_i => axi_request,
+        axi_response_o => axi_response,
+        phy_ca_o => phy_ca,
+        phy_dq_o => phy_dq_out,
+        phy_dq_i => phy_dq_in
+    );
+
+    ctrl_setup <= (
+        enable_refresh => '1',
+        priority_mode => '1',
+        priority_direction => '1'
+    );
+
+
+    -- Generate write requests
+    process
+        procedure do_write(address : natural) is
+        begin
+            axi_request.wa_address <= to_unsigned(address, 25);
+            axi_request.wa_byte_mask <= (others => '1');
+            axi_request.wa_valid <= '1';
+            loop
+                clk_wait;
+                exit when axi_response.wa_ready;
+            end loop;
+            axi_request.wa_address <= (others => 'U');
+            axi_request.wa_byte_mask <= (others => 'U');
+            axi_request.wa_valid <= '0';
+        end;
+
+    begin
+        axi_request.wa_valid <= '0';
+        axi_request.wal_valid <= '0';
+
+        clk_wait(5);
+
+        for n in 0 to 512 loop
+            do_write(n);
+        end loop;
+
+        wait;
+    end process;
+
+    -- Generate read requests
+    process
+        procedure do_read(address : natural) is
+        begin
+        end;
+    begin
+        axi_request.ra_valid <= '0';
+        axi_request.ral_valid <= '0';
+
+        wait;
+    end process;
+
+
+    decode : entity work.decode_commands port map (
+        clk_i => clk,
+        ca_command_i => ( ca => phy_ca.ca, ca3 => phy_ca.ca3 ),
+        tick_count_o => tick_count
+    );
+end;
---
+++ gddr6/sim/ctrl/do/compile.do
@@ -0,0 +1,60 @@
+# Paths from environment
+set vhd_dir $env(VHD_DIR)
+set common_vhd $env(COMMON_VHD)
+set bench_dir $env(BENCH_DIR)
+set common_sim $env(COMMON_SIM)
+
+vlib work
+vlib msim
+vlib msim/xil_defaultlib
+
+vcom -64 -2008 -work xil_defaultlib \
+    $common_vhd/support.vhd \
+    $common_vhd/util/short_delay.vhd \
+    $common_vhd/util/memory_array.vhd \
+    $common_vhd/util/long_delay.vhd \
+    $common_vhd/util/fixed_delay_dram.vhd \
+    $common_vhd/util/fixed_delay.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_command_defs.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_core_defs.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_timing_defs.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_read.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_write.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_lookahead.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_admin.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_refresh.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_bank.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_banks.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_request_mux.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_request.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_command.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl_data.vhd \
+    $vhd_dir/gddr6_defs.vhd \
+    $vhd_dir/ctrl/gddr6_ctrl.vhd
+
+vcom -64 -2008 -work xil_defaultlib \
+    $bench_dir/../../common/decode_commands.vhd \
+    $bench_dir/testbench.vhd
+
+vsim -t 1ps -voptargs=+acc -lib xil_defaultlib testbench
+
+view wave
+
+add wave -group "Read" ctrl/read/*
+add wave -group "Write" ctrl/write/*
+add wave -group "Lookahead" ctrl/lookahead/*
+add wave -group "Admin" ctrl/admin/*
+add wave -group "Refresh" ctrl/refresh/*
+add wave -group "Banks" ctrl/command/banks/*
+add wave -group "Mux" ctrl/command/request_mux/*
+add wave -group "Request" ctrl/command/request/*
+add wave -group "Command" ctrl/command/*
+add wave -group "Data" ctrl/data/*
+add wave -group "Ctrl" ctrl/*
+add wave -group "Bench" sim:*
+
+quietly set NumericStdNoWarnings 1
+
+run 10 us
+
+# vim: set filetype=tcl:
