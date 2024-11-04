ce0656fc59f648b83404af0a6eeb457c234b22b8
Michael Abbott <michael.abbott@diamond.ac.uk>
2024-11-04 09:31:17 +0000

    Tidy up build of IP
    Specify version (currently 0.0.0), include gddr6_defs.vhd and the register
    defines file, and move the built IP directory.

--- gddr6/tcl/create_ip.tcl
+++ gddr6/tcl/create_ip.tcl
@@ -8,31 +8,32 @@ set vhd_dir $env(VHD_DIR)
 set common_vhd $env(COMMON_VHD)
 
 set ip_dir $env(IP_DIR)
+set ip_version $env(IP_VERSION)
 
 
 # Prepare project to build netlist
 create_project $project_name $project_name -part $fpga_part
-
 set_param project.enableVHDL2008 1
 set_property target_language VHDL [current_project]
+
 
 # Add all the files listed in the source file
 set infile [open $fpga_top/gddr6-ip-files]
 while { [gets $infile line] >= 0 } {
-    set filename [subst $line]
-    puts $filename
-    add_files $filename
+    set newfile [add_files [subst $line]]
+    set_property FILE_TYPE "VHDL 2008" $newfile
 }
 close $infile
-set_property FILE_TYPE "VHDL 2008" [get_files *.vhd]
 set_property top gddr6_ip_netlist [current_fileset]
+
 
 # Build the design and write the netlist
 synth_design -flatten_hierarchy rebuilt -mode out_of_context
 write_checkpoint -force post_synth
-write_edif $ip_dir/src/gddr6_ip_netlist.edn
+write_edif $ip_dir/gddr6_ip/src/gddr6_ip_netlist.edn
 
 
 # Now prepare the IP.  First we need the interface definitions
-set_property ip_repo_paths $fpga_top/interface_ip [current_project]
-ipx::infer_core -vendor diamond.ac.uk -library user -taxonomy /UserIP $ip_dir
+set_property ip_repo_paths $ip_dir [current_project]
+ipx::infer_core -vendor diamond.ac.uk -library user -taxonomy /UserIP \
+    -version $ip_version $ip_dir/gddr6_ip
