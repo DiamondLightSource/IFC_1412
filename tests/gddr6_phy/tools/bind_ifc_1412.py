3d213e44090eabbd6fd995673b9f5f4427b7b4cb
Michael Abbott <michael.abbott@diamond.ac.uk>
2025-02-26 08:55:13 +0000

    Update gddr6_phy tools to use new binding framework

--- tests/gddr6_phy/tools/bind_ifc_1412.py
+++ tests/gddr6_phy/tools/bind_ifc_1412.py
@@ -1,8 +1,10 @@
 # Defines mapping to test-gddr6 registers
 
+import os
+import numpy
+
 from ifc_lib import defs_path
 from fpga_lib.driver import driver
-
 
 class Registers(driver.RawRegisters):
     NAME = 'ifc_1412-gddr6'
@@ -12,8 +14,9 @@ class Registers(driver.RawRegisters):
 
         register_defines = defs_path.register_defines(__file__)
         gddr6_defines = defs_path.gddr6_register_defines()
-
-        self.make_registers('SYS', None, gddr6_defines, register_defines)
+        lmk04616_defines = defs_path.lmk04616_defines()
+        self.make_registers('SYS', None,
+            gddr6_defines, lmk04616_defines, register_defines)
 
 def open(addr = 0):
     regs = Registers(addr)
