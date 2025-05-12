d8c9d866fab32dc33a3ddc6fa6f676a8cd9eeaee
Michael Abbott <michael.abbott@diamond.ac.uk>
2025-05-12 15:12:20 +0100

    Make types in board_fmc_swaps more explicit
    Turns out that although Vivado is happy with this syntax, ModelSim isn't.

--- board/IFC_1412/board_fmc_swaps.vhd
+++ board/IFC_1412/board_fmc_swaps.vhd
@@ -32,23 +32,23 @@ package body board_fmc_swaps is
     end;
 
 
-    constant FMC1_LA_SWAPS : std_ulogic_vector := make_swaps(34, (
+    constant FMC1_LA_SWAPS : std_ulogic_vector(33 downto 0) := make_swaps(34, (
         3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 15, 16, 19, 20, 22, 23, 24, 28
     ));
-    constant FMC1_HA_SWAPS : std_ulogic_vector := make_swaps(24, (
+    constant FMC1_HA_SWAPS : std_ulogic_vector(23 downto 0) := make_swaps(24, (
         2, 3, 4, 7, 8, 9, 10, 11, 14, 16, 19, 20, 21, 22, 23
     ));
-    constant FMC1_HB_SWAPS : std_ulogic_vector := make_swaps(22, (
+    constant FMC1_HB_SWAPS : std_ulogic_vector(21 downto 0) := make_swaps(22, (
         8, 9, 12, 14, 16, 18, 20, 21
     ));
 
-    constant FMC2_LA_SWAPS : std_ulogic_vector := make_swaps(34, (
+    constant FMC2_LA_SWAPS : std_ulogic_vector(33 downto 0) := make_swaps(34, (
         2, 4, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 27, 28, 29, 30, 31, 32, 33
     ));
-    constant FMC2_HA_SWAPS : std_ulogic_vector := make_swaps(24, (
+    constant FMC2_HA_SWAPS : std_ulogic_vector(23 downto 0) := make_swaps(24, (
         3, 6, 19, 20, 21, 22, 23
     ));
-    constant FMC2_HB_SWAPS : std_ulogic_vector := make_swaps(22, (
+    constant FMC2_HB_SWAPS : std_ulogic_vector(21 downto 0) := make_swaps(22, (
         9, 12, 14, 16, 18, 19, 20, 21
     ));
 end;
