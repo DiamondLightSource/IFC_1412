89a99708cbea60fd55b69ec82f991fa3fe1b7754
Michael Abbott <michael.abbott@diamond.ac.uk>
2025-05-15 10:13:02 +0100

    Fix bug in post bitstream hook
    On repeated runs of write bitstream need to use -force to overwrite any
    existing .bin files.

--- board/IFC_1412/post_bitstream.tcl
+++ board/IFC_1412/post_bitstream.tcl
@@ -1,4 +1,4 @@
 # Create .bin files from .bit file, to be run after bitstream generation
 
-write_cfgmem -format bin -size 128 -interface SPIx8 \
+write_cfgmem -format bin -size 128 -interface SPIx8 -force \
     -loadbit "up 0x00000000 top.bit" -file ../../../top.bin
