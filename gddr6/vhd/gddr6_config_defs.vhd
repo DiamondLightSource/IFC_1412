60f68fea5979ddfbfd4f1bd2b0074f0c4cabb334
Michael Abbott <michael.abbott@diamond.ac.uk>
2025-09-08 10:37:41 +0100

    Improve layout of gddr6_config_defs

--- gddr6/vhd/gddr6_config_defs.vhd
+++ gddr6/vhd/gddr6_config_defs.vhd
@@ -35,29 +35,34 @@ package gddr6_config_defs is
         (7,  2),    (7, 10),    (7,  3),    (7,  9),
         (6, 10),    (7,  7),    (6, 11),    (6,  6),
         (6,  7),    (6,  2),    (6,  3),    (6,  1),
-        (6,  0),    (7,  8),    (6,  5),    (6,  4));
+        (6,  0),    (7,  8),    (6,  5),    (6,  4)
+    );
     -- DBI
     constant CONFIG_BANK_DBI : pin_config_array_t(0 to 7) := (
         (0,  5),    (1,  4),    -- Bank 1 A
         (2,  9),    (3,  9),    -- Bank 1 B
         (5,  2),    (4,  3),    -- Bank 2 A
-        (7,  1),    (6,  9));   -- Bank 2 B
+        (7,  1),    (6,  9)     -- Bank 2 B
+    );
     -- EDC
     constant CONFIG_BANK_EDC : pin_config_array_t(0 to 7) := (
         (0,  0),    (1,  5),    -- Bank 1 A
         (2, 10),    (3,  5),    -- Bank 1 B
         (5,  8),    (4,  1),    -- Bank 2 A
-        (7, 11),    (7,  0));   -- Bank 2 B
+        (7, 11),    (7,  0)     -- Bank 2 B
+    );
     -- WCK
     constant CONFIG_BANK_WCK : pin_config_array_t(0 to 1) := (
         (1,  0),                -- Bank 1
-        (5,  0));               -- Bank 2
+        (5,  0)                 -- Bank 2
+    );
 
     -- Patch input for slices which need to be instantiated following the
     -- directions in UG571 (v1.14, p147): clock sink nibbles must instantiate
     -- bitslice 0.  As it happens, only pad_SG12_CK_P meets this description.
     constant CONFIG_BANK_PATCH : pin_config_array_t(0 to 0) := (
-        0 => (2,  0));          -- Connected to SG12_CK
+        0 => (2,  0)            -- Connected to SG12_CK
+    );
 
     -- Aggregate of all the configs above
     constant CONFIG_BANK_ALL : pin_config_array_t;
